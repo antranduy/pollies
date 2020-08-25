f84f0bff889f2b5969e74fd013f3edca3c9ddbb8
antranduy <45984092+antranduy@users.noreply.github.com>
2020-08-25 10:59:19 +1000

    Update 3_LE_politicians_pooled_sexes.R

--- R/3_LE_politicians_pooled_sexes.R
+++ R/3_LE_politicians_pooled_sexes.R
@@ -1,6 +1,6 @@
 #-----------------------------------------------------------------------------------------------------------------------
-# Purpose: Estimate life expectancy and 95% CI of politicians in different countries over consecutive 10-year periods,
-#          pooling males and females
+# Purpose: Estimate life expectancies and their 95% CIs of politicians in different countries over consecutive 
+#          10-year periods, pooling males and females
 # Author : An Tran-Duy - University of Melbourne
 # Date   : 1 February 2020
 # Place  : Melbourne
