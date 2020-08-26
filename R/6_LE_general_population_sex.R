f8482169c64969b8b45ead6441a026b82abd64d4
antranduy <45984092+antranduy@users.noreply.github.com>
2020-08-26 16:57:26 +1000

    Update 6_LE_general_population_sex.R

--- R/6_LE_general_population_sex.R
+++ R/6_LE_general_population_sex.R
@@ -11,9 +11,7 @@ library(dplyr)
 setwd('C:/Users/adtran/OneDrive - The University of Melbourne/Politicians longevity/GitHub/Data')
 
 expectancy_sex_long <- read.csv('life_expectancy_population_sex.csv', stringsAsFactors = FALSE)
-
 save(expectancy_sex_long, file = 'LifeExpectancyGeneralPopulation_sex_long.RData')
 
 expectancy_sex_wide <- reshape(expectancy_sex_long, idvar = c('country', 'years'), timevar = 'sex', v.name = 'expect', direction = 'wide' )
-
 save(expectancy_sex_wide, file = 'LifeExpectancyGeneralPopulation_sex_wide.RData')
