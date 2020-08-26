b5785bedcf5779a4640ed7940755341166b27ac5
antranduy <45984092+antranduy@users.noreply.github.com>
2020-08-26 16:54:46 +1000

    Update and rename 0_Country_list.R to 1_Country_list.R

--- R/1_Country_list.R
+++ R/1_Country_list.R
@@ -1,1 +1,2 @@
+# A vector of countries for reading input and writing output
 countries <- c('Austria','Australia','Canada', 'France','Germany','Italy', 'Netherlands','New_Zealand','Switzerland','UK','USA')
